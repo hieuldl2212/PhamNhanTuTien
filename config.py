6ce91cdfef12892d5048fec7c57466986b0d4174
hieuldl2212 <hieuldl2212@gmail.com>
2026-07-26 02:58:13 +0700

    Add GROQ_API_KEY to environment variable loading

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-
+GROQ_API_KEY = os.getenv('GROQ_API_KEY') 
 DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
 GEMINI_API_KEY = os.getenv('GEMINI_API_KEY') 
 PREFIX = '!'
